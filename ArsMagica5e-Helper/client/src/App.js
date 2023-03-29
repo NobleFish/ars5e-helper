e2cf48e89ddfcb4423a763be19e7f9361e92dbc4
NobleFish <sirnoblefish@gmail.com>
2023-03-29 02:09:19 -0500

    Updated references in App.js & navbar.js

--- ArsMagica5e-Helper/client/src/App.js
+++ ArsMagica5e-Helper/client/src/App.js
@@ -5,9 +5,9 @@ import { Route, Routes } from "react-router-dom";
  
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
-import CharacterList from "./components//characters/listCharacters";
-import Edit from "./components/edit";
-import Create from "./components/create";
+import CharacterList from "./components/characters/listCharacters";
+import EditCharacter from "./components/characters/editCharacter";
+import CreateCharacter from "./components/characters/createCharacter";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
@@ -17,8 +17,8 @@ const App = () => {
      <Navbar />
      <Routes>
        <Route exact path="/" element={<CharacterList />} />
-       <Route path="/edit/:id" element={<Edit />} />
-       <Route path="/create" element={<Create />} />
+       <Route path="/editCharacter/:id" element={<EditCharacter />} />
+       <Route path="/createCharacter" element={<CreateCharacter />} />
      </Routes>
    </div>
  );
