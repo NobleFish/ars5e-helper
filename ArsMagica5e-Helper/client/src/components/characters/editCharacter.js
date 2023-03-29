939e58f36b497c0e37e265231f341710a4baca9f
NobleFish <66657911+NobleFish@users.noreply.github.com>
2023-03-29 10:16:17 -0500

    Replaced template code in editCharacter

--- ArsMagica5e-Helper/client/src/components/characters/editCharacter.js
+++ ArsMagica5e-Helper/client/src/components/characters/editCharacter.js
@@ -1,144 +1,114 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router";
 
+
 const BASE_URL = process.env.REACT_APP_SERVER_URL+process.env.REACT_APP_SERVER_PORT;
 
-export default function Edit() {
-const [form, setForm] = useState({
-name: "",
-position: "",
-level: "",
-records: [],
-});
-const params = useParams();
-const navigate = useNavigate();
+export default function EditCharacter() {
+    
+    // Default values of form before character data collected
+    const BLANK_CHARACTER = {
+        character_name: "",
+        character_type: ""
+    }
+    
+    const [form, setForm] = useState(BLANK_CHARACTER);
 
-useEffect(() => {
-async function fetchData() {
-const id = params.id.toString();
-const response = await fetch(`${BASE_URL}/record/${params.id.toString()}`);
+    // Requirement to get value from previous page
+    const params = useParams();
 
-if (!response.ok) {
-const message = `An error has occurred: ${response.statusText}`;
-window.alert(message);
-return;
-}
+    // Requirement to navigate away
+    const navigate = useNavigate();
 
-const record = await response.json();
-if (!record) {
-window.alert(`Record with id ${id} not found`);
-navigate("/");
-return;
-}
+    // This fetches character data on page enter
+    useEffect(() => {
+        async function fetchData() {
+            // ID is being passed from previous page
+            const id = params.id.toString();
+            
+            // Request character response from server
+            const response = await fetch(`${BASE_URL}/character/${params.id.toString()}`);
 
-setForm(record);
-}
+            // If server does not respond
+            if (!response.ok) {
+                const message = `An error has occurred: ${response.statusText}`;
+                window.alert(message);
+                return;
+            }
 
-fetchData();
+            const character = await response.json();
+                if (!character) {
+                    window.alert(`Character with id ${id} not found`);
+                    navigate("/");
+                return;
+            }
 
-return;
-}, [params.id, navigate]);
+            setForm(character);
+        }
 
-// These methods will update the state properties.
-function updateForm(value) {
-return setForm((prev) => {
-return { ...prev, ...value };
-});
-}
+        fetchData();
 
-async function onSubmit(e) {
-e.preventDefault();
-const editedPerson = {
-name: form.name,
-position: form.position,
-level: form.level,
-};
+        return;
+    }, [params.id, navigate]);
 
-// This will send a post request to update the data in the database.
-await fetch(`${BASE_URL}/update/${params.id}`, {
-method: "POST",
-body: JSON.stringify(editedPerson),
-headers: {
-'Content-Type': 'application/json'
-},
-});
+    // These methods will update the state properties.
+    function updateForm(value) {
+        return setForm((prev) => {
+            return { ...prev, ...value };
+        });
+    }
 
-navigate("/");
-}
+    async function onSubmit(e) {
+       // Prevent default submission 
+        e.preventDefault();
+        
+        // Creates the editedCharacter object
+        const editedCharacter = {
+            character_name: form.name,
+            character_type: form.position
+        };
 
-// This following section will display the form that takes input from the user to update the data.
-return (
-<div>
-<h3>Update Record</h3>
-<form onSubmit={onSubmit}>
-<div className="form-group">
-<label htmlFor="name">Name: </label>
-<input
-type="text"
-className="form-control"
-id="name"
-value={form.name}
-onChange={(e) => updateForm({ name: e.target.value })}
-/>
-</div>
-<div className="form-group">
-<label htmlFor="position">Position: </label>
-<input
-type="text"
-className="form-control"
-id="position"
-value={form.position}
-onChange={(e) => updateForm({ position: e.target.value })}
-/>
-</div>
-<div className="form-group">
-<div className="form-check form-check-inline">
-<input
-className="form-check-input"
-type="radio"
-name="positionOptions"
-id="positionIntern"
-value="Intern"
-checked={form.level === "Intern"}
-onChange={(e) => updateForm({ level: e.target.value })}
-/>
-<label htmlFor="positionIntern" className="form-check-label">Intern</label>
-</div>
-<div className="form-check form-check-inline">
-<input
-className="form-check-input"
-type="radio"
-name="positionOptions"
-id="positionJunior"
-value="Junior"
-checked={form.level === "Junior"}
-onChange={(e) => updateForm({ level: e.target.value })}
-/>
-<label htmlFor="positionJunior" className="form-check-label">Junior</label>
-</div>
-<div className="form-check form-check-inline">
-<input
-className="form-check-input"
-type="radio"
-name="positionOptions"
-id="positionSenior"
-value="Senior"
-checked={form.level === "Senior"}
-onChange={(e) => updateForm({ level: e.target.value })}
-/>
-<label htmlFor="positionSenior" className="form-check-label">Senior</label>
-</div>
-</div>
-<br />
+        // This will send a post request to update the character in the database.
+        await fetch(`${BASE_URL}/character/update/${params.id}`, {
+            method: "POST",
+            body: JSON.stringify(editedCharacter),
+            headers: {
+            'Content-Type': 'application/json'
+            }
+        });
 
-<div className="form-group">
-<input
-type="submit"
-value="Update Record"
-className="btn btn-primary"
-/>
-</div>
-</form>
-</div>
-);
+        navigate("/");
+    }
+
+    // This following section will display the form that takes input from the user to update the data.
+    return (
+        <div>
+            <h3>Update Record</h3>
+            <div className="form-group">
+                <label htmlFor="character_name">Character Name</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="character_name"
+                    value={form.character_name}
+                    onChange={(e) => updateForm({ character_name: e.target.value })}
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="character_type">Character Type</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="character_type"
+                    value={form.character_type}
+                    onChange={(e) => updateForm({ character_type: e.target.value })}
+                />
+            </div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input type="submit" value="Update Record" className="btn btn-primary"/>
+                </div>
+            </form>
+        </div>
+    );
 }
