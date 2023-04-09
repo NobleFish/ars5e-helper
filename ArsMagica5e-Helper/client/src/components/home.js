4399deccae659a83983f04780bfb315c4f89fb93
NobleFish <sirnoblefish@gmail.com>
2023-04-08 22:56:43 -0500

    Improved visuals of navbar and home

--- ArsMagica5e-Helper/client/src/components/home.js
+++ ArsMagica5e-Helper/client/src/components/home.js
@@ -1,10 +1,11 @@
 import React from "react";
+
 import "../styling/home.css";
 
 
 export default function HomePage(){
     return(
-        <div>
+        <div className="homeContainer">
             <h1>Ars Magica Helper App</h1>
         </div>
     )
--- ArsMagica5e-Helper/client/src/components/navbar.js
+++ ArsMagica5e-Helper/client/src/components/navbar.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import '../styling/navbar.css'
  
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
@@ -7,38 +8,56 @@ import "bootstrap/dist/css/bootstrap.css";
 import { NavLink } from "react-router-dom";
  
 // Here, we display our Navbar
-export default function Navbar() {
- return (
-   <div>
-     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-       <NavLink className="navbar-brand" to="/">
-       <img style={{"width" : 25 + '%'}} src="./arm5-logo.png"></img>
-       </NavLink>
-       <button
-         className="navbar-toggler"
-         type="button"
-         data-toggle="collapse"
-         data-target="#navbarSupportedContent"
-         aria-controls="navbarSupportedContent"
-         aria-expanded="false"
-         aria-label="Toggle navigation"
-       >
-         <span className="navbar-toggler-icon"></span>
-       </button>
- 
-       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul className="navbar-nav ml-auto">
-           <li className="nav-item">
-             <NavLink className="nav-link" to="/characters">
+function Navbar() 
+{
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  
+  const showButton = () => {
+    if(window.innerWidth <= 960){
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <nav className='navbar'>
+          <NavLink to='/' onClick={closeMobileMenu}>
+            <img className='img' src='/arm5-logo.png'></img>
+          </NavLink>
+        <div className='navbar-container'>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? "fa-regular fa-x" : "fa-solid fa-bars"} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <NavLink to='/characters' className='nav-links' onClick={closeMobileMenu}>
                 Characters
-             </NavLink>
-             <NavLink className="nav-link" to="/covenants">
+              </NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink to='/covenants' className='nav-links' onClick={closeMobileMenu}>
                 Covenants
-             </NavLink>
-           </li>
-         </ul>
-       </div>
-     </nav>
-   </div>
- );
+              </NavLink>
+            </li>
+            
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
 }
+
+export default Navbar
