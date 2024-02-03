aae30dcf4cbe20100de8559aec4bd3a6bb86864f
NobleFish <sirnoblefish@gmail.com>
2024-02-02 21:25:35 -0500

    Added new page links

--- ArsMagica5e-Helper/client/src/App.js
+++ ArsMagica5e-Helper/client/src/App.js
@@ -16,11 +16,16 @@ import {CovenantForm} from "./components/covenants/covenantForm";
 
 import { PrintCharacter } from "./components/characters/print/printCharacter";
 
+import { SoloHome } from "./components/play_tools/solo_home";
+
+import { NotFound } from "./components/utility/PageNotFound";
+
 const App = () => {
  return (
    <div>
      <Navbar />
      <Routes>
+       <Route path='*' element={<NotFound />} />
        <Route exact path="/" element={<HomePage />} />
        <Route path="/characters" element={<CharacterList />}/>
        <Route path="/editCharacter/:id" element={<CharacterForm queryType={"edit"} />} />
@@ -28,7 +33,8 @@ const App = () => {
        <Route path="/covenants" element={<CovenantList />}/>
        <Route path="/editCovenant/:id" element={<CovenantForm queryType={"edit"} />} />
        <Route path="/createCovenant" element={<CovenantForm queryType={"add"} />} />
-       <Route path="/print/character" element={<PrintCharacter />} />
+       <Route path="/print/character/:id" element={<PrintCharacter />} />
+       <Route path="/tools/solo" element={<SoloHome />} /> 
      </Routes>
    </div>
  );
